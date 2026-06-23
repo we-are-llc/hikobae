a3c1d5e6369c2f129a4f07568064da4a945c6173
Masato Azechi <masato.azechi@we-re.net>
2026-06-23 10:23:23 +0900

    TEST

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,8 +41,7 @@ export default function Home({ onNavigate }) {
     <div className="home">
       <div className="home-logo">🌱</div>
       <div style={{ textAlign: 'center' }}>
-        <div className="home-title">ひこばえ</div>
-        <div className="home-subtitle">こたえを かこう</div>
+        <div className="home-title">ひこばえ</div>        
       </div>
 
       <div className="home-buttons">
